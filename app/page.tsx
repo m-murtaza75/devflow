101df374bbee70340437f7d0f45c6c59ff1d2b25
m-murtaza75 <m.murt75@gmail.com>
2024-12-16 17:28:08 +0000

    ESLint & Prettier setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <h1>Nextjs</h1>    
-  );
+  return <h1 className="text-3xl font-black text-white">Nextjs</h1>;
 }
